ed41b299bab690551febcfb15bad05548288a879
Ehsan <esadrfa@g.clemson.edu>
2019-05-24 13:13:50 -0400

    remove and add the academic theme

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
+echo "building the website using hugo command..."
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
@@ -15,9 +16,12 @@ msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
+
+echo "before commit "
 git commit -m "$msg"
 
 # Push source and build repos.
+echo "before push to remote repo "
 git push origin master
 
 # Come Back up to the Project Root
